5f50f0698bd735e9fd964efd36ae86b971f4e885
joelchooks <joelchukks@gmail.com>
2022-03-13 22:09:59 +0100

    FIX[Fixed Model User]

--- app/core/models.py
+++ app/core/models.py
@@ -40,7 +40,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return f'{self.username}'
