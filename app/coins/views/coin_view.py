1159cd0e0ffb37d9e9316bd0a4115673e18257e1
augustine.jibunoh <jibunoh.augustine@gmail.com>
2022-04-29 10:28:14 +0100

    fix: [A bug fix on consuming CoinAPI endpoint]

--- app/coins/views/coin_view.py
+++ app/coins/views/coin_view.py
@@ -1,3 +1,6 @@
+import requests
+
+from decouple import config
 from rest_framework.generics import ListAPIView
 from rest_framework import status
 
@@ -12,9 +15,25 @@ class CoinAPIView(ListAPIView):
     serializer_class = CoinSerializer
 
     def list(self, request):
-        """Return a list of available coin"""
-        coins = self.get_queryset()
-        serializer = self.get_serializer(coins, many=True)
-        return Response(
-            data=dict(coins=serializer.data, total=len(serializer.data)),
-            status=status.HTTP_200_OK)
+        """Return a list of available coins"""
+        try:
+            url = 'https://rest.coinapi.io/v1/assets'
+            headers = {'X-CoinAPI-Key': config(
+                'X_CoinAPI_Key', default='618D9FBA-CE44-4B44-89B1-284D80D70746', cast=str)}
+            response = requests.get(url, headers=headers)
+
+            get_all_coins = []
+
+            for data in response.json():
+                get_all_coins.append(
+                    {
+                        'name': data.get('asset_id'),
+                        'USD-PRICE': data.get('price_usd'),
+                        'volume': data.get('volume_1hrs_usd')
+                    })
+
+                if len(get_all_coins) == 3:
+                    break
+            return Response(data={'coins': get_all_coins}, status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response(errors={'error': str(err)}, status=status.HTTP_400_BAD_REQUEST)
