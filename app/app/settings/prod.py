588fe7e4ad32988ac0e10aca6ef76cbf7e3c6e0a
Nathan-Yinka <oludarenathaniel@gmail.com>
2024-06-28 05:27:38 +0100

    FIX[static file]

--- app/app/settings/prod.py
+++ app/app/settings/prod.py
@@ -12,12 +12,14 @@ ALLOWED_HOSTS = ["*"]
 
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.environ.get('POSTGRES_DB'),
-       'USER': os.environ.get('POSTGRES_USER'),
-       'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-       'HOST': os.environ.get("DATABASE_HOST"),
-       'PORT': 5432,
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': '5432',
+    }
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- dockerfile
+++ dockerfile
@@ -1,16 +1,12 @@
-FROM python:3.8.12-alpine
-LABEL Inyang Kpongette
+FROM python:3.10.6
 
 ENV PYTHONUNBUFFERED 1
 
-COPY ./requirements.txt /requirements.txt
-RUN apk add --no-cache --virtual .build-deps gcc musl-dev \
-    && pip install --no-cache-dir -r /requirements.txt \
-    && apk del .build-deps
-RUN pip install flake8 flake8-docstrings restructuredtext-lint
-RUN mkdir /app
 WORKDIR /app
-COPY ./app /app
 
-RUN adduser -D user
-USER user
+COPY ./requirements.txt /requirements.txt
+RUN pip install -r /requirements.txt 
+RUN pip install flake8 flake8-docstrings restructuredtext-lint
+
+
+COPY . .
