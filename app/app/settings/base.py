9d3cd86f2840f4b0eff400e07b2f1cf2a1afb532
Nathan-Yinka <oludarenathaniel@gmail.com>
2024-07-01 00:37:05 +0100

    CREATED[add search and pagination function to the product api view]

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     
+    
 ]
 
 MIDDLEWARE = [
--- app/product/views.py
+++ app/product/views.py
@@ -1,10 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
+from app.utilis import ProductPagePagination
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = ProductPagePagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'description']
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
